8c88c23879f803f8619959df00f6059e58c9be97
swaroop-500apps <swaroop.barre@500apps.com>
2023-04-11 15:04:05 +0530

    added chart

---
+++ components/collections/Barchart.vue
@@ -0,0 +1,28 @@
+<template>
+    <Bar id="mychartid" />
+</template>
+  
+<script setup>
+ import {onMounted, ref , defineProps } from 'vue';
+ import Chart from 'chart.js/auto';
+ const mychartid = ref(null);
+ let BarInstance = null;
+ const props = defineProps({
+    options: {
+      type: Boolean,
+      required: true,
+    },
+    data: {
+      type: Object,
+      required: true,
+    },
+  });
+  onMounted(() => {
+    const Bar = mychartid.value;
+    BarInstance = new Chart(Bar, {
+      type: 'Grid Chart',
+      data: props.data,
+      options:props.data,
+    });
+  });
+</script>
---
+++ components/collections/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <collectionsBarchart :options="chartOptions" :data="chartData" />
+</template>
+  
+<script setup>
+import { ref, defineProps, defineEmits } from vue
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const chartData = {
+  labels: ['January', 'February', 'March'],
+  datasets: [{ data: [40, 20, 12] }]
+}
+  
+const chartOptions = true
+
+</script>
